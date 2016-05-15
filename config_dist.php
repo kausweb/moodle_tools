d356edf9bb8f4ffa6cb76df0d72cf59b7eb2655b
kausweb <kaushtuvgurung@gmail.com>
2016-05-15 14:11:40 +1000

    added gitignore to ignore the config file

---
+++ config_dist.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Config file for tools
+ *
+ * @package    tools
+ * @copyright  &copy; 2016 CG Kineo {@link http://www.kineo.com}
+ * @author     kaushtuv.gurung
+ * @version    1.0
+ */
+
+$config = new stdClass();
+
+$config->author = 'kaushtuv.gurung';
+$config->moodleversion = 2014110400;
+$config->projectpath = 'c:/htdocs'; 
